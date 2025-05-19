236bec3e2c21ffa814245eedc12e520b7bb35ae0
Jason <jasonpavlas@icloud.com>
2025-05-19 17:43:06 -0400

    C

--- WebSite/studio-world-client/src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-// This file exports all UI components for easier imports
-export { default as Alert } from './Alert';
-export { default as Button } from './Button';
-export { default as InputField } from './InputField';
